2738590d19ea7451d43db8b42414a5228a02042c
Prashant Cholachagudda <pvc@outlook.com>
2022-02-28 16:14:40 +0530

    updated the project to dotnet core

--- SearchScorer/SearchScorer/Common/SearchServiceClientExtensions.cs
+++ SearchScorer/SearchScorer/Common/SearchServiceClientExtensions.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Azure.Search;
 using Microsoft.Azure.Search.Models;
+using Index = Microsoft.Azure.Search.Models.Index;
 
 namespace SearchScorer.Common
 {
